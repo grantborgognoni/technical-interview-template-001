c3c516b155deb9e0b2ac873b169cbc2bf3904629
StudioAstera <grantborgognoni@gmail.com>
2025-05-24 00:39:12 -0500

    feat: add sticky sidebar and header

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -8,11 +8,15 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1">{children}</main>
+    <div className="flex min-h-screen flex-col bg-gray-50">
+      <div className="sticky top-0 z-10">
+        <Header />
+      </div>
+      <div className="flex flex-1">
+        <div className="sticky top-16 h-[calc(100vh-4rem)]">
+          <Sidebar />
+        </div>
+        <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
     </div>
   );
