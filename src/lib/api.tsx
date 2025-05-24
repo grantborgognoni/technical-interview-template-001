4a95d1da6099b341c804df13b583218110254c93
StudioAstera <grantborgognoni@gmail.com>
2025-05-24 00:30:46 -0500

    feat: add promise all to fetch all dashboard reqs concurrently, decrease LCP from 0.14 to 0.07

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -6,34 +6,32 @@ import type { User } from "~/pages/api/users";
 
 export async function fetchDashboardData() {
   try {
-    const usersResponse = await fetch("/api/users").then(
-      (res) => res.json() as Promise<User[]>,
-    );
-    const projectsResponse = await fetch("/api/projects").then(
-      (res) => res.json() as Promise<{ data: Project[] }>,
-    );
-    const activitiesResponse = await fetch("/api/activities").then(
-      (res) => res.json() as Promise<{ activities: Activity[] }>,
-    );
-    const metricsResponse = await fetch("/api/metrics").then(
-      (res) => res.json() as Promise<Metrics>,
-    );
-    const eventsResponse = await fetch("/api/events").then(
-      (res) => res.json() as Promise<{ upcoming: Event[] }>,
-    );
+    const [users, projects, activities, metrics, events] = await Promise.all([
+      fetch("/api/users").then((res) => res.json() as Promise<User[]>),
+      fetch("/api/projects").then(
+        (res) => res.json() as Promise<{ data: Project[] }>,
+      ),
+      fetch("/api/activities").then(
+        (res) => res.json() as Promise<{ activities: Activity[] }>,
+      ),
+      fetch("/api/metrics").then((res) => res.json() as Promise<Metrics>),
+      fetch("/api/events").then(
+        (res) => res.json() as Promise<{ upcoming: Event[] }>,
+      ),
+    ]);
 
     const mergedData = {
-      users: usersResponse,
-      projects: projectsResponse,
-      recentActivity: activitiesResponse.activities.slice(0, 5),
+      users: users,
+      projects: projects,
+      recentActivity: activities.activities.slice(0, 5),
       metrics: {
-        activeUsers: metricsResponse.users.active,
-        completedProjects: metricsResponse.projects.completed,
-        pendingTasks: projectsResponse.data.reduce(
+        activeUsers: metrics.users.active,
+        completedProjects: metrics.projects.completed,
+        pendingTasks: projects.data.reduce(
           (acc: number, project: Project) => acc + project.pendingTasks,
           0,
         ),
-        upcomingEvents: eventsResponse.upcoming.filter(
+        upcomingEvents: events.upcoming.filter(
           (event: Event) => new Date(event.date) > new Date(),
         ).length,
       },
